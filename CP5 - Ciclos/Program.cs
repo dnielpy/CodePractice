195497443c78a167c6c57fc9feab2d1a04528704
dnielpy2 <dnielquesada@gmail.com>
2023-10-11 23:40:48 -0400

    parte de la 5

---
+++ CP5 - Ciclos/Program.cs
@@ -0,0 +1,18 @@
+﻿//Implemente un metodo que devuelva el mayor elemento de un array
+int[] x = {1,109,2,4,12,4,56,3,99,-3};
+/*
+int Mayor(int[] x){
+    int contador = 0;
+
+    for (int i = 0; i < x.Length; i++)
+    {
+        if (x[contador] < x[i])
+        {
+            contador = i;
+        }
+    }
+    return x[contador];
+}
+
+System.Console.WriteLine(Mayor(x));
+*/
