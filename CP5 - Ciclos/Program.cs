ddfc257b164a8619269dbcb8b588f73139d0576a
dnielpy2 <dnielquesada@gmail.com>
2023-10-11 23:43:04 -0400

    5 aun

--- CP5 - Ciclos/Program.cs
+++ CP5 - Ciclos/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Implemente un metodo que devuelva el mayor elemento de un array
-int[] x = {1,109,2,4,12,4,56,3,99,-3};
 /*
+int[] x = {1,109,2,4,12,4,56,3,99,-3};
 int Mayor(int[] x){
     int contador = 0;
 
