a16121fc7d9e1c6f7c4e9b3da2a135a9752f1ced
dnielpy2 <dnielquesada@gmail.com>
2023-11-08 12:54:42 -0500

    Anoder tanda beibi tandaa

--- CP8 - Ciclos (may the for be with you)/Program.cs
+++ CP8 - Ciclos (may the for be with you)/Program.cs
@@ -26,6 +26,7 @@ MenorDiferencia(a);
 */
 
 //Criba de Erat´ostenesEscriba un m´etodo que dado un entero n devuelva un array de bool de tama˜non.El array resultante debe tener valor true en la posici´on i si el n´umero enteroi es primo. En caso contrario, tendr´a valor f alse.Por ejemplo, para n = 10, el array resultante debe ser: true, true, f alse,f alse, true, f alse, true, f alse, true, true
+/*
 bool[] Primo(int x){
     bool[] clasificacion = new bool[x + 1];
     for (int i = 0; i < clasificacion.Length; i++)
@@ -37,9 +38,33 @@ bool[] Primo(int x){
     }
     return clasificacion;
 }
-
 Primo(5);
+*/
 
 
-
+//¿Cu´antos n´umeros?Escriba un m´etodo que reciba un entero k y un array de valores y devuevlacu´antos de esos valores se repiten exactamente k veces.
+/*
+int[] Repeticiones(int k, int[] x){
+    List<int> repeticiones = new List<int>();
+    for (int i = 0; i < x.Length; i++)
+    {
+        int contador = 0;
+        for (int t = 0; t < x.Length; t++)
+        {
+            if (x[i] == x[t])
+            {
+                contador++;
+            }
+        }
+        if (contador == k)
+        {
+            repeticiones.Add(x[i]);
+            break;
+        }
+    }
+    return repeticiones.ToArray();
+}
+int[] a = {1,2,34,6,7,7,7};
+Repeticiones(3, a);
+*/
 
