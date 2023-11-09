e9653f9956aaf5e18bea5015cd4eb9c955e480b4
dnielpy2 <dnielquesada@gmail.com>
2023-11-08 22:26:58 -0500

    jijijaja

--- CP10 - Funciones recursivas que invocan funciones recursivas que invocan/Program.cs
+++ CP10 - Funciones recursivas que invocan funciones recursivas que invocan/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Factorial Implemente un m´etodo que calcule recursivamente el factorial de un n´umero entero n.
+/*int factorial(int x){
+    if (x == 1)
+    {
+        return x;
+    }
+    else
+    {
+        return x * factorial(x - 1);
+    }
+}
+System.Console.WriteLine(factorial(4));*/
+
+//FibonacciImplemente un m´etodo que calcule recursivamente el n-´esimo n´umero de lasecuencia de Fibonacci.
+/*int Fibonacci(int x){
+    if (x == 0)
+    {
+        return 0;
+    }
+    if (x == 1)
+    {
+        return x;
+    }
+    else
+    {
+        return x + Fibonacci(x-1);
+    }
+}
+System.Console.WriteLine(Fibonacci(4));*/
+
+//Suma de elementos Implemente un m´etodo que sume recursivamente todos los elementos de una lista de enteros.
+/*int SumaElementos(List<int> x){
+    if (x.Count == 1)
+    {
+        return x[0];
+    }
+    else
+    {
+        int bridge = x.Last();
+        x.Remove(x.Last());
+        return bridge + SumaElementos(x);
+    }
+}
+List<int> a = new List<int>();
+a.Add(1);
+a.Add(3);
+System.Console.WriteLine(SumaElementos(a));*/
