338b48dbdc166a11e0b345904d2fd2f4d56888d7
dnielpy2 <dnielquesada@gmail.com>
2023-11-13 19:49:11 -0500

    Primera de la cp2 de recursividad hecha

--- CP11 - Más recursión, con algo de polimorfismo/Program.cs
+++ CP11 - Más recursión, con algo de polimorfismo/Program.cs
@@ -1,1 +1,22 @@
-﻿//Pal´ındromoImplemente una funci´on recursiva que reciba un string y analice si este espal´ındromo.
+﻿//Pal´ındromo Implemente una funci´on recursiva que reciba un string y analice si este es pal´ındromo.
+bool Palindromo(string x){
+    string y = "";
+    if(x == "" && y == ""){
+        return true;
+    }
+    for (int i = 0; i < x.Length; i++)
+    {
+        y += x[x.Length - 1 - i];
+    }
+
+    if (x[0] != y[y.Length-1])
+    {
+        return false;
+    }
+    else
+    {
+        return Palindromo(x.Remove(x.Length - 1));
+    }
+}
+
+System.Console.WriteLine(Palindromo("radar"));
