161903c0685bf9b8e50b6b8c634c0ef1ecc15134
dnielpy2 <dnielquesada@gmail.com>
2024-02-03 12:39:51 -0500

    tengo a lo mio hablando italiano de luneavierne solo comen pata

---
+++ Primeros TC/EstabilizarGenes/C1XX - Nombre y Apellidos/Program.cs
@@ -0,0 +1,29 @@
+﻿string Estabilizar(string cadena){
+    List<string>solucion = new List<string>();
+
+    int repeticiones = cadena.Length / 4;
+
+    for (int x = 0; x < cadena.Length; x++)
+    {
+        int contador = 0;
+        for (int y = 0; y < cadena.Length; y++)
+        {
+            if (cadena[x] == cadena[y])
+            {
+                contador++;
+                if (contador > repeticiones)
+                {
+                solucion.Add(cadena[y].ToString());
+                contador = 0;
+                }
+            }
+            
+        }
+    }
+
+
+    return solucion.ToString();
+}
+
+
+Estabilizar("AATTGGGA");
