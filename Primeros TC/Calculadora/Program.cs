33b0385b9bf0d8ca9f831de1a15cbd54fc1d71b4
dnielpy <dnielquesada@gmail.com>
2024-03-13 16:52:58 -0400

    Calculadora hecha

---
+++ Primeros TC/Calculadora/Program.cs
@@ -0,0 +1,44 @@
+﻿int Calculadora(char[] y){
+    int resultado = 0;
+    List<char> x = y.ToList();
+
+    int toInt(char t){
+        return (int)Char.GetNumericValue(t);
+    } 
+
+    for (int i = 0; i < x.Count; i++)
+    {
+        if (x[i] == '+')
+        {
+            resultado = toInt(x[i-1]) + toInt(x[i+1]);
+            x.RemoveRange(0, i+2);
+            x.Insert(0, resultado.ToString()[0]);
+            i = 0;
+        }
+        if (x[i] == '-')
+        {
+            resultado = toInt(x[i-1]) - toInt(x[i+1]);
+            x.RemoveRange(0, i+2);
+            x.Insert(0, resultado.ToString()[0]);
+            i = 0;
+        }
+        if (x[i] == '*')
+        {
+            resultado = toInt(x[i-1]) * toInt(x[i+1]);
+            x.RemoveRange(0, i+2);
+            x.Insert(0, resultado.ToString()[0]);
+            i = 0;
+        }
+        if (x[i] == 'C')
+        {
+            x.RemoveRange(0, i+1);
+            x.Insert(0, 0.ToString()[0]);
+            i = 0;
+        }
+    }
+    return resultado;
+}
+
+char[] a = {'1', '+', '2', '+', '1', 'C', '1', '+', '2'};
+
+Calculadora(a);
