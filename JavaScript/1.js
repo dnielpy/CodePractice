c2fd4ab8a7bfad4e1ec45681902c0705b9fe5377
dnielpy <dnielquesada@gmail.com>
2024-04-24 01:50:14 -0400

    Some Javascript problems added

---
+++ JavaScript/1.js
@@ -0,0 +1,12 @@
+/*var Saludo = "Hola Mundo ;)";
+console.log(Saludo);*/
+
+//Muestre en la consola el valor maximo y el valor mınimo admitidos por el tipo int.
+// let max = Number.MAX_SAFE_INTEGER;
+// let min = Number.MIN_SAFE_INTEGER;
+// console.log(max, min);
+
+//Muestre en la consola un valor aproximado de Pi (use Math).
+// let pi = Math.PI;
+
+
