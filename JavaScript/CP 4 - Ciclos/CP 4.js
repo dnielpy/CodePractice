b0cacdd84d46981624ad392924e360cabdb4f116
dnielpy <dnielquesada@gmail.com>
2024-05-02 15:51:10 -0400

    start with java

---
+++ JavaScript/CP 4 - Ciclos/CP 4.js
@@ -0,0 +1,33 @@
+//Implemente un m´etodo que devuelva el mayor elemento de un array
+// function Mayor(x){
+//     let mayor = 0;
+//     for (let i = 0; i < x.length; i++) {
+//         if (x[i] > mayor) {
+//             mayor = x[i];
+//         }
+//     }
+//     return mayor;    
+// }
+// const miArray = [20, 2, 3, 4, 5];
+// console.log(Mayor(miArray));
+
+//Implemente un m´etodo que devuelva el segundo menor elemento de un array.
+// function SegundoMenor(x){
+//     let menor = Number.MAX_SAFE_INTEGER;
+//     let segundomenor = Number.MAX_SAFE_INTEGER;
+//     for (let i = 0; i < x.length; i++) {
+//         if (x[i] < menor) {
+//             menor = x[i];
+//         }
+//     }
+//     for (let i = 0; i < x.length; i++) {
+//         if (x[i] < segundomenor && x[i] != menor) {
+//             segundomenor = x[i];
+//         }
+//     }
+//     return segundomenor;    
+// }
+// const miArray = [20, 2, 3, 4, 5];
+// console.log(SegundoMenor(miArray));
+
+
