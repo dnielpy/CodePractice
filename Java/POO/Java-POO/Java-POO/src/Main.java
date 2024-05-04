95024cd846ade3aac5fe7afdd8007ce7e747ef48
dnielpy <dnielquesada@gmail.com>
2024-05-03 20:13:12 -0400

    Password Generator done

--- Java/POO/Java-POO/Java-POO/src/Main.java
+++ Java/POO/Java-POO/Java-POO/src/Main.java
@@ -1,145 +1,74 @@
-import javax.sound.midi.SysexMessage;
-import java.util.Random;
+import java.security.SecureRandom;
 import java.util.Scanner;
 
-class Person{
-    protected String name = "";
-    protected int age = 0;
-    protected int id;
-    protected String gender = "M";
-    protected double height = 0;
-    protected double weight = 0;
+class Password{
+    private int lenght = 8;
+    private String password;
 
-    public Person(){
-        id = generateID();
+    public Password(String Password){
+        password = Password;
+    }
+    public Password(int Lenght){
+        //Generate password with Lenght;
+        password = generatePassword(Lenght);
+        lenght = Lenght;
     }
 
-    public Person(String Name, int Age, String Gender){
-        name = Name;
-        age = Age;
-        gender = Gender;
-        id = generateID();
-    }
-    public Person(String Name, int Age, String Gender, double Height, double Weight){
-        name = Name;
-        age = Age;
-        gender = Gender;
-        height = Height;
-        weight = Weight;
-        id = generateID();
-    }
-    public int checkBMI(){  //-1, 0, 1
-        double BMI =  weight/height;
-        if (BMI < 20) {
-            return -1;
+    //generates the password for the object with its current length.
+    public String generatePassword(int len) {
+        final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < len; i++) {
+            int randomIndex = random.nextInt(chars.length());
+            sb.append(chars.charAt(randomIndex));
         }
-        if (BMI >= 20 && BMI <= 25) {
-            return  0;
-        }
-        else {
-            return 1;
-        }
+        return sb.toString();
     }
-    public boolean isAdult(){
-        if (age >= 18){
+
+    //returns a boolean indicating whether it is strong or not. To be strong, it must have more than 2 uppercase letters, more than 1 lowercase letter, and more than 5 numbers.
+    public boolean isStrong(){
+        int mayusculas = (int) password.chars().filter(Character::isUpperCase).count();
+        int minusculas = (int) password.chars().filter(Character::isLowerCase).count();
+        int numeros = (int) password.chars().filter(Character::isDigit).count();
+
+        if (mayusculas >= 2 && minusculas >= 1 && numeros >= 5) {
             return true;
         }
         else {
             return false;
         }
     }
-    public String checkGender(){
-        if (gender == "M") {
-            return "M";
-        }
-        else {
-            return "F";
-        }
-    }
-    public int generateID(){
-        Random random = new Random();
-        int rand = random.nextInt(100000);
-        return  rand;
-    }
 
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", id=" + id +
-                ", gender='" + gender + '\'' +
-                ", height=" + height +
-                ", weight=" + weight +
-                '}';
+    //Get and set
+    public String getPassword() {
+        return password;
     }
-
-    public String getName(){
-        return name;
+    public int getLenght() {
+        return lenght;
     }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("¿Cuál es tu nombre? ");
-        String nombre = entrada.nextLine();
-        System.out.print("Ingresa la edad: ");
-        int edad = entrada.nextInt();
-        System.out.print("Ingresa el genero: ");
-        String genero = entrada.nextLine();
-        System.out.print("Ingresa el peso: ");
-        int peso = entrada.nextInt();
-        System.out.print("Ingresa la altura: ");
-        int altura = entrada.nextInt();
+        System.out.print("Ingresa el tamaño: ");
+        int user_lenght = entrada.nextInt();
 
-        Person person1 = new Person(nombre, edad, genero, peso, altura);
-        Person person2 = new Person("daniel", 21, "M");
-        Person person3 = new Person();
+        Password[] passwordsObjects = new Password[user_lenght];
+        boolean[] secure_of_passwords = new boolean[user_lenght];
 
-        person3.setName("camion");
-        person3.setAge(50);
-        person3.setGender("M");
-        person3.setHeight(10.0);
-        person3.setWeight(20.0);
-
-        System.out.printf(String.valueOf(person1.toString()));
-        System.out.printf(String.valueOf(person2.toString()));
-        System.out.printf(String.valueOf(person3.toString()));
+        for (int i = 0; i < passwordsObjects.length; i++) {
+            Password temp = new Password(user_lenght);
+            passwordsObjects[i] = temp;
+            secure_of_passwords[i] = temp.isStrong();
+            String toShow =  passwordsObjects[i].getPassword() + "   " + String.valueOf(secure_of_passwords[i]);
+            System.out.println(toShow);
+        }
     }
 }
 
