ef341fb36960ba6ddb7267c0c5c0f388260e7139
dnielpy <reynaldo.dquesada@estudiantes.matcom.uh.cu>
2024-05-09 17:53:13 -0400

    Added some SOLID metodologies to Roeadmap

--- 10 - Spring/demo/src/main/java/com/example/demo/student/StudentController.java
+++ 10 - Spring/demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,4 +1,5 @@
 package com.example.demo.student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentservice) {
         this.studentService = studentservice;
     }
